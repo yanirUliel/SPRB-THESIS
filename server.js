2df8b0391b2d7a2a4dc091b89cdf2252109394e4
יניר אוליאל <yanir8181@gmail.com>
2025-11-24 09:46:06 +0200

    Fix public path

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const path = require('path');
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// הגשה של קבצים סטטיים מהתיקייה public
+app.use(express.static(path.join(__dirname, "public")));
 
-// Serve the index.html file for any route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
